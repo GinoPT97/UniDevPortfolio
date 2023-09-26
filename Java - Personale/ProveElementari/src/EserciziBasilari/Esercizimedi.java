108f974ff2bfc6ca4605f5ea7d4d0544412e7e9a
Kenobi1797 <g.pandozzitrani@studenti.unina.it>
2023-09-26 09:05:23 +0200

    Aggiunta Classe per nuovi esercizi

---
+++ Java - Personale/ProveElementari/src/EserciziBasilari/Esercizimedi.java
@@ -0,0 +1,13 @@
+package EserciziBasilari;
+
+import java.util.Scanner;
+
+public class Esercizimedi {
+	
+	static Scanner input = new Scanner(System.in); // input
+
+	public static void main(String[] args) {
+
+	}
+
+}
