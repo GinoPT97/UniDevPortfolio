4ac6e4ff3d3c089aecdcfb821b5985dfa954d031
Kenobi1797 <g.pandozzitrani@studenti.unina.it>
2023-09-26 18:24:21 +0200

    piazzamento codice

---
+++ Android-Studio/RandomChatClient-1/app/src/main/java/com/rmc/randomchat/MainActivity.java
@@ -0,0 +1,184 @@
+package com.rmc.randomchat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rmc.randomchat.depinjection.MyApplication;
+import com.rmc.randomchat.net.RandomChatRepository;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+     private EditText NicknameUser;
+     private Button button_start_chat;
+     CheckBox checkBox;
+     RandomChatRepository randomChatRepository = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        isConnected();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NicknameUser = findViewById(R.id.name_user);
+        checkBox = findViewById(R.id.checkBox_nickname);
+        checkBox.setVisibility(View.VISIBLE);
+
+
+        SharedPreferences username = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = username.getString("Nick", "");
+        if(name != null && !name.equals("")) {
+            checkBox.setEnabled(true);
+            checkBox.setAlpha(1.0f);
+            NicknameUser.setText(name);
+        }else{
+            checkBox.setEnabled(false);
+            checkBox.setAlpha(0.4f);
+        }
+
+
+        NicknameUser.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(checkBox.isChecked()) {
+                    SharedPreferences.Editor editor = username.edit();
+                    editor.putString("Nick", NicknameUser.getText().toString());
+                    editor.apply();
+                }
+                if(NicknameUser.getText().toString().isEmpty()){
+                    runOnUiThread(() -> {
+                        if(checkBox.isChecked())
+                            checkBox.toggle();
+                        checkBox.setEnabled(false);
+                        checkBox.setAlpha(0.4f);
+                    });
+
+                }else
+                    runOnUiThread(() -> {
+                        checkBox.setEnabled(true);
+                        checkBox.setAlpha(1.0f);
+                    });
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        checkBox.setOnClickListener(v -> {
+            SharedPreferences.Editor editor = username.edit();
+            if(checkBox.isChecked())
+                editor.putString("Nick", NicknameUser.getText().toString());
+            editor.apply();
+        });
+
+        button_start_chat = findViewById(R.id.button_start_chat);
+
+        button_start_chat.setOnClickListener(v -> {
+
+            SharedPreferences.Editor editor = username.edit();
+
+            if(!EditTextisEmpty(NicknameUser)){
+                if(!checkBox.isChecked()){
+                    editor.remove("Nick");
+                    editor.apply();
+                }
+                String nickname = NicknameUser.getText().toString();
+                if (nickname.getBytes().length > 20){
+                    NicknameUser.setError("Nickname Troppo lungo");
+                    return;
+                } else if(nickname.trim().isEmpty()){
+                    NicknameUser.setError("Inserisci un Nickname");
+                    return;
+                }
+                if(randomChatRepository != null){
+                    AsyncTask.execute(() -> {
+                        try {
+                            randomChatRepository.setNickname(nickname);
+
+                            Intent roomRecyclerView = new Intent(MainActivity.this, ActivityRoom.class);
+                            roomRecyclerView.putExtra("nickname", nickname);
+
+                            startActivity(roomRecyclerView);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            runOnUiThread(() -> NetworkErrorFactory.newNetworkError(this, "Errore di connessione al server, riavvia l'applicazione", NetworkErrorFactory.TYPE.TERMINATE).show());
+                        }
+                    });
+                } else {
+                    isConnected();
+                }
+            }
+        });
+    }
+
+
+    private void isConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()){
+            if(networkInfo.isConnected()) {
+                if (randomChatRepository == null) {
+                    randomChatRepository = ((MyApplication) getApplication()).appContainer.randomChatRepository;
+                    AsyncTask.execute(() -> {
+                        try {
+                            randomChatRepository.connect();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            randomChatRepository = null;
+                            runOnUiThread(() -> NetworkErrorFactory.newNetworkError(this, "Impossibile connettersi al server, prova a riavviare l'applicazione o ritenta più tardi.", NetworkErrorFactory.TYPE.TERMINATE).show());
+                        }
+                    });
+                }
+            }
+        } else {
+            NetworkErrorFactory.newNetworkError(this, "Internet non disponibile, Controlla la tua connessione e riprova", NetworkErrorFactory.TYPE.CONNECT).show();
+        }
+    }
+
+
+    public boolean EditTextisEmpty(EditText nicknameuser) {
+        if (TextUtils.isEmpty(nicknameuser.getText())) {
+            nicknameuser.setError("Nickname non inserito!");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        isConnected();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isConnected();
+    }
+
+}
---
+++ Android-Studio/RandomChatClient-1/app/src/main/java/com/rmc/randomchat/net/RandomChatRepositoryImpl.java
@@ -0,0 +1,200 @@
+package com.rmc.randomchat.net;
+
+import android.util.Log;
+
+import com.rmc.randomchat.entity.Room;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
+
+public class RandomChatRepositoryImpl implements RandomChatRepository, Serializable {
+
+    private RandomChatClient client;
+
+    private boolean chatting = false;
+
+    private final Consumer<ChatListener> chatThreadFun = (chatListener) -> {
+        String msg = " ";
+        try {
+            while (chatting || msg.charAt(0) != 'x'){
+                msg = client.readLine();
+
+                switch (msg.charAt(0)){
+                    case Commands.ENTER_IN_ROOM:
+                        chatListener.onUserFound(stringInside(msg, "[", "]"));
+                        break;
+                    case Commands.SEND_MSG:
+                        chatListener.onMessage(msg.substring(2));
+                        break;
+                    case Commands.NEXT_USER:
+                        chatListener.onNextUser();
+                        break;
+                    case Commands.TIME_EXPIRED:
+                        chatListener.onTimeExpired();
+                        break;
+                    case Commands.EXIT:
+                        chatListener.onExit();
+                        break;
+                    case Commands.USERS_IN_ROOM:
+                        chatListener.onUsersCount(Long.parseLong(msg.substring(2).trim()));
+                        break;
+                    case Commands.EXIT_FROM_ROOM:
+                        return;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + msg.charAt(0));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    };
+
+    Thread chatThread = null;
+
+    @Override
+    public void connect() throws IOException{
+        client = RandomChatClient.getInstance();
+        client.openConnection();
+    }
+
+    @Override
+    public void setNickname(String nickname) throws IOException{
+        client.write(Commands.CHANGE_NICKNAME, "[" + nickname + "]");
+    }
+
+    @Override
+    public List<Room> getAllRooms() throws IOException{
+        return getRooms(0, 0, "");
+    }
+
+    @Override
+    public List<Room> getRooms(int from, int to) throws IOException{
+        return getRooms(from, to, "");
+    }
+
+    @Override
+    public List<Room> getRoomsByName(String name) throws IOException{
+        return getRooms(0, 0, name);
+    }
+
+    private List<Room> getRooms(int from, int to, String name) throws IOException, SocketTimeoutException {
+        if(chatThread != null)
+            try{
+                chatThread.join();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+        String msg = from + " " +  to + " [" +  name + "]";
+        List<Room> list = new ArrayList<>();
+
+        client.write(Commands.ROOM_LIST, msg);
+
+        int numRooms = Integer.parseInt(client.readLine(5 * 1000).substring(2).trim());
+
+        String room;
+        int i = 0;
+        while (i < numRooms && (room = client.readLine(5 * 1000)) != null){
+            Log.println(Log.DEBUG, "SERVER", "RoomString: " + room);
+            Scanner roomScanner = new Scanner(room.substring(1));
+
+            long id = roomScanner.nextLong();
+            long usersCount = roomScanner.nextLong();
+            int  roomColor = roomScanner.nextInt();
+            int time = roomScanner.nextInt();
+            String roomName = stringInside(room, "[", "]");
+
+            Room r = new Room(roomName, id, time, usersCount, roomColor);
+            list.add(r);
+            Log.println(Log.DEBUG, "SERVER", "Room Added: " + r.toString());
+            ++i;
+        }
+        return list;
+    }
+
+    @Override
+    public Room addRoom(Room room) throws IOException, SocketTimeoutException{
+        String msg = room.getRoomColor() + " " + room.getTime() + " [" + room.getName() + "]";
+        client.write(Commands.NEW_ROOM, msg);
+
+        String roomStr = client.readLine(10 * 1000);
+        if (roomStr == null) return null;
+        else return new Room(room.getName(), Long.parseLong(roomStr.substring(2).trim()), room.getTime(), 0, room.getRoomColor());
+    }
+
+    @Override
+    public String enterRoom(Room room, ChatListener chatListener) throws IOException, RoomNotExistsException {
+        client.write(Commands.ENTER_IN_ROOM, room.getId() + "");
+
+        do{
+            String msg = client.readLine();
+            if(msg != null){
+                if (msg.charAt(0) == Commands.EXIT) throw new RoomNotExistsException();
+                if(msg.charAt(0) == Commands.EXIT_FROM_ROOM) return null;
+
+                if(msg.charAt(0) == Commands.USERS_IN_ROOM) chatListener.onUsersCount(Long.parseLong(msg.substring(2).trim()));
+                else {
+                    chatListener.onUserFound(stringInside(msg, "[", "]"));
+                    chatThread = new Thread(() -> chatThreadFun.accept(chatListener));
+                    chatting = true;
+                    chatThread.start();
+
+                    return msg.substring(2);
+                }
+            }
+        } while (true);
+
+    }
+
+    @Override
+    public void sendMessage(String msg) throws IOException {
+        client.write(Commands.SEND_MSG, msg.replace("\n", " "));
+    }
+
+    @Override
+    public void nextUser() throws IOException{
+        client.write(Commands.NEXT_USER, "");
+    }
+
+    @Override
+    public void exitRoom() throws IOException {
+        if(!chatting){
+            System.out.println("Sending Exit");
+            exit();
+        } else {
+            chatting = false;
+            exit();
+            try {
+                System.out.println("JOIN chatThread");
+                chatThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("ChatThread Terminated");
+        }
+        System.out.println("Exit From Room.");
+    }
+
+     @Override
+    public void exit() throws IOException{
+        client.write(Commands.EXIT, "");
+    }
+
+    @Override
+    public void getUserCount() throws IOException{
+        client.write(Commands.USERS_IN_ROOM, "");
+    }
+
+
+    private static String stringInside(String s, String left, String right){
+        Log.println(Log.DEBUG, "ROOM", "Stringa da dividere: " + s);
+        if (s == null || s.length() < 3 || !s.contains(left) || !s.contains(right)) return "???";
+        else return s.split(Pattern.quote(left))[1].split(Pattern.quote(right))[0];
+    }
+}
