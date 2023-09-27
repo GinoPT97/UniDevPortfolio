f03080013d3b508e0de0494ff09d43c772f3473e
Kenobi1797 <g.pandozzitrani@studenti.unina.it>
2023-09-27 09:52:46 +0200

    Piazzamento esercizi server

---
+++ C-LSO/RandomChat-Server-2/src/FileManagement/fileManager.h
@@ -0,0 +1,13 @@
+#ifndef RANDOMCHATSERVER_FILEMANAGER_H
+#define RANDOMCHATSERVER_FILEMANAGER_H
+#include "../datastructures/vector.h"
+
+// return substring len
+int stringInside(const char* in, char left, char right, char* out, int maxLen);
+
+void loadFromFile(RoomVector* vec, const char* filename);
+
+pthread_t startAutoSave(RoomVector* vec, const char* filename, int period); // period in seconds
+
+
+#endif
---
+++ C-LSO/RandomChat-Server-2/src/Finder/connection.c
@@ -0,0 +1,46 @@
+#include "connection.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+
+Connection* createConnection(User* user){
+    Connection* conn = malloc(sizeof(Connection));
+    conn->user1 = user;
+    conn->user2 = NULL;
+    conn->status = -1;
+    conn->mutex = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+    pipe(conn->pipefd);
+    fcntl(conn->pipefd[0], F_SETFL, O_NONBLOCK); // set non block reading;
+    conn->timer = NULL;
+    return conn;
+}
+
+void setConnectionTimeout(Connection* conn, unsigned int timeout){
+    conn->timer = newTimer(timeout, conn->pipefd[1]);
+}
+
+void connectUser(Connection* conn, User* user){
+    conn->user2 = user;
+    conn->status++;
+    char c = 'c';
+    write(conn->pipefd[1], &c, 1);
+}
+
+void closeConnection(Connection* conn){
+    pthread_mutex_lock(&conn->mutex);
+    conn->status++;
+    pthread_mutex_unlock(&conn->mutex);
+
+    if(conn->status == 2) {
+        close(conn->pipefd[0]);
+        close(conn->pipefd[1]);
+        if(conn->timer != NULL) deleteTimer(conn->timer);
+        free(conn);
+    }
+}
+
+int isOpen(const Connection* conn){
+    return conn->status == 0 && (conn->timer == NULL || !conn->timer->isEnd);
+}
