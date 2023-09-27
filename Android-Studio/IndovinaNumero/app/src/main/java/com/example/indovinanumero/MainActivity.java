b83c317deaa87ea117adac2564be2d2756a8b441
Kenobi1797 <g.pandozzitrani@studenti.unina.it>
2023-09-27 09:25:29 +0200

    Piccole modifiche

--- Android-Studio/IndovinaNumero/app/src/main/java/com/example/indovinanumero/MainActivity.java
+++ Android-Studio/IndovinaNumero/app/src/main/java/com/example/indovinanumero/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.indovinanumero;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button changeactivity = changeactivity = findViewById(R.id.cambiobutton);
+
+        changeactivity.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        });
     }
 }
