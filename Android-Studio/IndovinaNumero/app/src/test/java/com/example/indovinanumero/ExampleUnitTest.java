72afc300c5d4ec309ebeb159ed84f83a32b19352
Kenobi1797 <g.pandozzitrani@studenti.unina.it>
2023-09-27 08:56:27 +0200

    Piccole modifiche

--- Android-Studio/IndovinaNumero/app/src/test/java/com/example/indovinanumero/ExampleUnitTest.java
+++ Android-Studio/IndovinaNumero/app/src/test/java/com/example/indovinanumero/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.example.provaapp;
-
-import static org.junit.Assert.assertEquals;
+package com.example.indovinanumero;
 
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
