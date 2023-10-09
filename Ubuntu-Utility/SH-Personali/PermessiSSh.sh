baa53f847694538b0023e2d71b186e6fd98b8416
Kenobi1797 <g.pandozzitrani@studenti.unina.it>
2023-10-09 12:05:47 +0200

    Create PermessiSSh.sh

---
+++ Ubuntu-Utility/SH-Personali/PermessiSSh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Modifica le autorizzazioni sul file chiave SSH privata
+chmod 600 ~/.ssh/id_rsa
+
+# Aggiungi la chiave SSH privata all'agente SSH
+ssh-add ~/.ssh/id_rsa
+
+# Verifica che la chiave SSH privata sia stata aggiunta all'agente SSH
+ssh-add -l
+
