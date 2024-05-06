a39332c88cfd434a526d5598dc525cb212cef5df
Kenobi1797 <g.pandozzitrani@studenti.unina.it>
2024-05-06 09:29:43 +0200

    Update Ubuntu-Utility/SH-Personali/InstallApp.sh script with system updates and package installations

--- Ubuntu-Utility/SH-Personali/InstallApp.sh
+++ Ubuntu-Utility/SH-Personali/InstallApp.sh
@@ -4,14 +4,14 @@
 sudo apt update && sudo apt upgrade -y
 
 # Installazione di pacchetti di sistema essenziali
-sudo apt install wget curl deborphan zram-config preload flatpak git gparted gpart openjdk-11-jdk openjdk-11-jre clamav clamtk postgresql-15 postgresql-client-15 postgresql-client-common postgresql-common codeblocks gnome-boxes arduino telegram-desktop vlc cmake deja-dup discord docker eclipse pycharm-community skype swi-prolog teams-for-linux vlc whatsie zoom-client libnvidia-gl-535:i386 tor -y
+sudo apt install wget curl deborphan zram-config preload flatpak git gparted gpart openjdk-11-jdk openjdk-11-jre clamav clamtk postgresql-15 postgresql-client-15 postgresql-client-common postgresql-common codeblocks gnome-boxes arduino vlc cmake deja-dup libnvidia-gl-535:i386 tor -y
 
 # Rimozione di Firefox e installazione di Google Chrome
-sudo apt-get remove --purge firefox && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb
+# sudo apt-get remove --purge firefox && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # Installazione di GitHub Desktop
-sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.8.1-linux2/GitHubDesktop-linux-2.8.1-linux2.deb
-sudo dpkg -i GitHubDesktop-linux-2.8.1-linux2.deb
+# sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.8.1-linux2/GitHubDesktop-linux-2.8.1-linux2.deb
+# sudo dpkg -i GitHubDesktop-linux-2.8.1-linux2.deb
 
 # Installazione di applicazioni tramite Snap
 sudo snap install --classic code
@@ -29,12 +29,21 @@ sudo snap install --classic swi-prolog
 sudo snap install --classic teams-for-linux
 sudo snap install --classic whatsie
 sudo snap install --classic zoom-client
+sudo snap install --classic spotify
 
 # Configurazione di pgAdmin 4
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" | sudo tee /etc/apt/sources.list.d/pgadmin4.list
 sudo apt update && sudo apt install pgadmin4 -y
 sudo /usr/pgadmin4/bin/setup-web.sh
+
+# Configurazione della password dell'utente PostgreSQL
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'admin';"
+
+#Installazione dello store basato su Flutter
+#sudo snap install flutter --classic
+#sudo snap remove snap-store
+#sudo snap install snap-store --channel=preview/edge
 
 # Esecuzione dello script per gli aggiornamenti personalizzati
 sudo /home/kenobi/Documenti/GitHub/CodicePersonale/Ubuntu-Utility/SH-Personali/Aggiornamenti.sh
