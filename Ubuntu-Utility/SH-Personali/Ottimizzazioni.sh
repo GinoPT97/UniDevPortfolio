8d936f15935f06cba79d19b05ce1bf9524893b43
Kenobi1797 <g.pandozzitrani@studenti.unina.it>
2024-05-06 09:47:53 +0200

    Update Ubuntu-Utility/SH-Personali/Ottimizzazioni.sh script to improve performance and cleanup

--- Ubuntu-Utility/SH-Personali/Ottimizzazioni.sh
+++ Ubuntu-Utility/SH-Personali/Ottimizzazioni.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 # Ottieni i permessi
-sudo journalctl --vacuum-files
+sudo journalctl --vacuum-files=1G
 
 # Pulisci i file temporanei
 sudo rm -rf /tmp/*
 
-# Svuota la cache DNS
-sudo systemd-resolve --flush-caches
-
-# Pulisci la cache dei font
+# Svuota la cache dei font
 sudo fc-cache -f -v
 
 # Pulisci i file di registro di sistema compressi
 sudo journalctl --vacuum-time=7d
 
-# Pulisci la cache del browser
+# Pulisci la cache del browser (nota: comando specifico per Google Chrome)
+# Assicurati di sostituire con il comando corretto per il tuo browser, se necessario
 google-chrome --clear-cache
 
-# Pulisci i file di registro di Apache
-sudo rm /var/log/apache2/*.log
+# Pulisci i file di registro di Apache solo se esistono
+sudo rm -f /var/log/apache2/*.log
 
-# Compatti e deframmenta i file di sistema
+# Compatta e deframmenta i file di sistema
 sudo e4defrag /
 
 # Riparazione dei file danneggiati
