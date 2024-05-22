45e07eb01938a08e705641479a884585ddce83ca
Kenobi1797 <gino.pt17@gmail.com>
2024-05-22 17:58:37 +0200

    Update Ubuntu-Utility/SH-Personali/InstallApp.sh script with additional package installations

--- Ubuntu-Utility/SH-Personali/InstallApp.sh
+++ Ubuntu-Utility/SH-Personali/InstallApp.sh
@@ -4,8 +4,8 @@
 sudo apt update && sudo apt upgrade -y
 
 # Installazione di pacchetti di sistema essenziali
-sudo apt install wget curl deborphan kate zram-config preload flatpak git gparted gpart default-jre openjdk-11-jdk openjdk-11-jre clamav clamtk postgresql-16 postgresql-client-16 postgresql-client-common postgresql-common codeblocks gnome-boxes arduino vlc cmake deja-dup libnvidia-gl-535:i386 tor -y
-sudo apt install postgresql postgresql-contrib curl ca-certificates -y
+sudo apt install -f wget curl deborphan kate zram-config preload flatpak git gparted gpart default-jre openjdk-11-jdk openjdk-11-jre clamav clamtk postgresql-16 postgresql-client-16 postgresql-client-common postgresql-common codeblocks gnome-boxes arduino vlc cmake deja-dup libnvidia-gl-535:i386 tor -y
+sudo apt install -f postgresql postgresql-contrib curl ca-certificates -y
 
 # Rimozione di Firefox, installazione di GitHub Desktop e installazione di Google Chrome
 # Rimozione di Firefox
@@ -41,13 +41,13 @@ sudo snap install --classic spotify
 sudo snap install --classic cmake
 
 # Configurazione di pgAdmin 4
-curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
-echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" | sudo tee /etc/apt/sources.list.d/pgadmin4.list
-sudo apt update && sudo apt install pgadmin4 -y
-sudo /usr/pgadmin4/bin/setup-web.sh
+#curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+#echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" | sudo tee /etc/apt/sources.list.d/pgadmin4.list
+#sudo apt update && sudo apt install pgadmin4 -y
+#sudo /usr/pgadmin4/bin/setup-web.sh
 
 # Configurazione della password dell'utente PostgreSQL
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'admin';"
+#sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'admin';"
 
 #Installazione dello store basato su Flutter
 #sudo snap install flutter --classic
