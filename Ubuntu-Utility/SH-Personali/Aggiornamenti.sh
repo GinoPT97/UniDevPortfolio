ddcce020a71497f532993d1f6fefe6e23c8eb763
Kenobi1797 <gino.pt17@gmail.com>
2024-05-29 10:14:47 +0200

    Update Ubuntu-Utility/SH-Personali/Aggiornamenti.sh and Ubuntu-Utility/SH-Personali/Ottimizzazioni.sh scripts

--- Ubuntu-Utility/SH-Personali/Aggiornamenti.sh
+++ Ubuntu-Utility/SH-Personali/Aggiornamenti.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# Funzione per gestire gli errori
+handle_error() {
+    echo "Errore alla linea $1"
+    exit 1
+}
+
+# Trap per catturare errori e chiamare la funzione handle_error
+trap 'handle_error $LINENO' ERR
+
 # Riparazione dei pacchetti APT
 sudo dpkg --configure -a
 
 # Aggiornamento dei pacchetti APT
 sudo apt update && sudo apt upgrade -y
 
-# Aggiornamento dei pacchetti e delle dipendenze 
+# Aggiornamento dei pacchetti e delle dipendenze
 sudo apt-get dist-upgrade -y
 
 # Per avanzamenti di sistema
-sudo do-release-upgrade 
+sudo do-release-upgrade -f DistUpgradeViewNonInteractive
 
 # Blocca la sospensione energetica del wifi
 sudo rfkill unblock wifi
@@ -18,10 +27,9 @@ sudo rfkill unblock all
 
 # Pulizia dei pacchetti inutili e orfani APT
 sudo apt autoremove --purge -y
-sudo apt-get autoremove --purge -y 
 
 # Pulizia della cache dei pacchetti APT
-sudo apt clean -y
+sudo apt clean
 
 # Aggiornamento dei pacchetti Snap
 sudo snap refresh
