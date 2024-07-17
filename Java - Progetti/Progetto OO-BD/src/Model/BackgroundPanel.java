f7a8b792af1919c8f631e10f42a2e4f50c34c7ab
Kenobi1797 <gino.pt17@gmail.com>
2024-07-17 17:02:02 +0200

    Aggiornamento di precaricamento

--- Java - Progetti/Progetto OO-BD/src/Model/BackgroundPanel.java
+++ Java - Progetti/Progetto OO-BD/src/Model/BackgroundPanel.java
@@ -2,35 +2,58 @@ package Model;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Color;
 import java.io.IOException;
 import java.net.URL;
-
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.SwingWorker;
 
 public class BackgroundPanel extends JPanel {
-	private Image backgroundImage;
+    private Image backgroundImage;
+    private boolean isImageLoaded = false;
+    
+    public BackgroundPanel(String imagePath) {
+        loadBackgroundImage(imagePath);
+    }
 
-	public BackgroundPanel(String imagePath) {
-		// Carica l'immagine utilizzando getClassLoader().getResource(), che restituisce
-		// un URL
-		URL imageURL = getClass().getClassLoader().getResource(imagePath);
-		if (imageURL == null) {
-			throw new IllegalArgumentException("L'immagine non è stata trovata: " + imagePath);
-		}
+    private void loadBackgroundImage(String imagePath) {
+        new SwingWorker<Image, Void>() {
+            @Override
+            protected Image doInBackground() throws Exception {
+                URL imageURL = getClass().getClassLoader().getResource(imagePath);
+                if (imageURL == null) {
+                    throw new IllegalArgumentException("L'immagine non è stata trovata: " + imagePath);
+                }
+                return ImageIO.read(imageURL);
+            }
 
-		try {
-			backgroundImage = ImageIO.read(imageURL);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            @Override
+            protected void done() {
+                try {
+                    backgroundImage = get();
+                    isImageLoaded = true;
+                    repaint();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.execute();
+    }
 
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		if (backgroundImage != null) {
-			g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
-		}
-	}
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (isImageLoaded && backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        } else {
+            // Placeholder: colore di sfondo o immagine di segnaposto
+            g.setColor(Color.LIGHT_GRAY);
+            g.fillRect(0, 0, getWidth(), getHeight());
+        }
+    }
 }
+
+
+
+
