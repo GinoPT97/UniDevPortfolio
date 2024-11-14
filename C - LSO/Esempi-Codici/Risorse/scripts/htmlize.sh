0a35fddbde0699546a5620e49815db1da679e0b2
Kenobi1797 <gino.pt17@gmail.com>
2024-11-14 12:16:53 +0100

    Correggi i percorsi nei file di script e aggiorna il Makefile per riflettere la nuova struttura delle directory

--- C - LSO/Esempi-Codici/Risorse/scripts/htmlize.sh
+++ C - LSO/Esempi-Codici/Risorse/scripts/htmlize.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## htmlize.sh
 ##
-## Copyright (C) 2003 Mirko Maischberger.  
+## Copyright (C) 2003 Mirko Maischberger.
 #*   This program is free software; you can redistribute it and/or modify
 #*   it under the terms of the GNU General Public License as published by
 #*   the Free Software Foundation; either version 2 of the License, or
@@ -14,25 +14,25 @@
 #*
 #*   You should have received a copy of the GNU General Public License
 #*   along with this program; if not, write to the Free Software
-#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 #*   02111-1307  USA
 ##
 ## Script per la compilazione del GaPiL con htlatex
-## ./htmlize.sh   
+## ./htmlize.sh
 ##    genera i file HTML ed estrae le immagini
 ##
 ## ./htmlize.sh  -p
 ##    genera i file HTML ma non estrae le immagini
 ##
 ## $Id: htmlize.sh,v 1.2 2003/04/23 22:09:52 piccardi Exp $
-## 
+##
 
 # compila usando il file di configurazione htgapil.cfg
 htlatex gapil htgapil " " "$1"
 
 # rimuove gli width e height dalle immagini, l'impostazione
-# di default di htlatex (che non so cambiare) è di usare la
-# dimesione dell'immagine in punti pica.
+# di default di htlatex (che non so cambiare) Ã¨ di usare la
+# dimensione dell'immagine in punti pica.
 perl -i.orig -pe 's/width=\".*\"//i' *.html
 perl -i.orig -pe 's/height=\".*\"//i' *.html
 perl -i.orig -pe 's/width=\".*\"//i' *.html
@@ -41,26 +41,30 @@ perl -i.orig -pe 's/height=\".*\"//i' *.html
 # ripristina il backslash negli esempi di codice HTML
 perl -i.orig -pe 's/INSERT_BACKSLASH_MAGIC8745/\\/g' *.html
 
-# sostitiusci il colore delle note (mouseover)
+# sostituisci il colore delle note (mouseover)
 perl -i.orig -pe 's/:aqua/:#fcc/g' *.css
 
-# sposta e pulisci
-mv *.html htgapil
-mv *.png htgapil
-mv *.js htgapil
-mv *.css htgapil
-# - rm *.html~
-# - rm *.html.orig
+# Sposta i file generati nella directory htgapil
+mv *.html htgapil/
+mv *.png htgapil/
+mv *.js htgapil/
+mv *.css htgapil/
 
-# piccola gabola per immagini mal convertite da htlatex
+# Rimuove i file temporanei creati da htlatex
+rm *.html~
+rm *.html.orig
+
+# Piccola correzione per le immagini mal convertite da htlatex
 convert -density 110x110 -geometry 70% img/tcpip_overview.ps htgapil/gapil117x.png
 convert -density 110x110 -geometry 50% img/iso_tcp_comp.ps htgapil/gapil114x.png
 convert -density 110x110 -geometry 50% img/tcp_data_flux.ps htgapil/gapil116x.png
 
+# Entra nella cartella di destinazione
 cd htgapil
-# wwis è uno script che riscrive width e height 
-# corretti nelle immagini
+
+# Esegui lo script wwwis per correggere width e height delle immagini
 wwwis *.html
-# pulizia
-# - rm *~
+
+# Pulizia finale: rimuove i file temporanei
+rm *~
 cd ..
--- C - LSO/Esempi-Codici/htmakeindex.sh
+++ C - LSO/Esempi-Codici/htmakeindex.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 ## Genera l'indice con i collegamenti ipertestuali
 ## per tex4ht
-tex '\def\filename{{gapil}{ind}{idx}{in}{out}} \input  idxmake.4ht'
+
+# Assicurati che il percorso di 'idxmake.4ht' sia corretto
+tex '\def\filename{{gapil}{ind}{idx}{in}{out}} \input Risorse/idxmake.4ht'
+
+# Assicurati che i file gapil.in e gapil.out siano nella posizione corretta
 makeindex -o gapil.out gapil.in
