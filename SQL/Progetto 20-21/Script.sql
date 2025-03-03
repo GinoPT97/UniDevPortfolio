f78c578543259cbe0d4905c238a31725f1024d5b
Kenobi1797 <gino.pt17@gmail.com>
2025-03-03 12:08:43 +0100

    Aggiorna le definizioni delle tabelle SQL per utilizzare chiavi primarie auto-incrementate e migliorare la coerenza dei controlli di validità

--- SQL/Progetto 20-21/Script.sql
+++ SQL/Progetto 20-21/Script.sql
@@ -1,53 +1,52 @@
--- Definizione della tabella cliente
+-- (Opzionale) Definizione del tipo TIPOLOGIA se non esiste già:
+CREATE TYPE TIPOLOGIA AS ENUM ('Ortofrutticoli', 'Latticini', 'Inscatolati', 'Farinacei');
 
-CREATE TABLE cliente(
-    codcliente VARCHAR(5) PRIMARY KEY,CHECK(CodCliente ~* '^[0-9]+$'),
-    nome VARCHAR(255) NOT NULL, CHECK(Nome ~* '^[A-Za-z ]+$'),
-    cognome VARCHAR(255) NOT NULL,CHECK(Cognome ~* '^[A-Za-z ]+$'),
-    codicefiscale CHAR(16) NOT NULL, CHECK(CodiceFiscale ~* '^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$'),
+-- Tabella cliente
+CREATE TABLE IF NOT EXISTS cliente (
+    codcliente SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL CHECK (nome ~* '^[A-Za-z ]+$'),
+    cognome VARCHAR(255) NOT NULL CHECK (cognome ~* '^[A-Za-z ]+$'),
+    codicefiscale CHAR(16) NOT NULL CHECK (codicefiscale ~* '^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$'),
     indirizzo VARCHAR(255) NOT NULL,
-    telefono VARCHAR(20) NOT NULL , CHECK(Telefono ~* '^[0-9+ ]+$'),
-    email VARCHAR(255) NOT NULL, CHECK(Email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-za-z]+$')
+    telefono VARCHAR(20) CHECK (telefono ~* '^[0-9+ ]+$'),
+    email VARCHAR(255) NOT NULL CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]+$')
 );
 
--- Definizione della tabella dipendente
-
-CREATE TABLE dipendente(
-    coddipendente VARCHAR(5) PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL, CHECK(Nome ~* '^[A-Za-z ]+$'),
-    cognome VARCHAR(255) NOT NULL,CHECK(Cognome ~* '^[A-Za-z ]+$'),
-    codicefiscale CHAR(16) NOT NULL, CHECK(CodiceFiscale ~* '^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$'),
+-- Tabella dipendente
+CREATE TABLE IF NOT EXISTS dipendente (
+    coddipendente SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL CHECK (nome ~* '^[A-Za-z ]+$'),
+    cognome VARCHAR(255) NOT NULL CHECK (cognome ~* '^[A-Za-z ]+$'),
+    codicefiscale CHAR(16) NOT NULL CHECK (codicefiscale ~* '^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$'),
     indirizzo VARCHAR(255) NOT NULL,
-    telefono VARCHAR(20) NOT NULL , CHECK(Telefono ~* '^[0-9+ ]+$'),
-    email VARCHAR(255) NOT NULL, CHECK(Email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-za-z]+$')
+    telefono VARCHAR(20) CHECK (telefono ~* '^[0-9+ ]+$'),
+    email VARCHAR(255) NOT NULL CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]+$')
 );
 
--- Definizione della tabella tessera
-
-CREATE TABLE tessera(
-        CodTessera VARCHAR(5) PRIMARY KEY, CHECK(CodTessera ~* '^[0-9]+$'),
-	NumeroPunti NUMERIC NOT NULL DEFAULT 0,
-	CodCliente VARCHAR(5) NOT NULL UNIQUE CHECK(CodCliente ~* '^[0-9]+$'),
-	CONSTRAINT TesseraFK FOREIGN KEY(CodCliente) REFERENCES CLIENTE(CodCliente)
+-- Tabella tessera
+CREATE TABLE IF NOT EXISTS tessera (
+    codtessera SERIAL PRIMARY KEY,
+    numeropunti REAL NOT NULL DEFAULT 0.00,
+    codcliente INTEGER NOT NULL UNIQUE,
+    CONSTRAINT TesseraFK FOREIGN KEY (codcliente) REFERENCES cliente (codcliente)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
--- Definizione della tabella prodotto
-
-CREATE TABLE prodotto(
-    codprodotto VARCHAR(5) PRIMARY KEY, CHECK(CodProdotto ~* '^[0-9]+$'),
-    nome VARCHAR(255) NOT NULL, CHECK(Nome ~* '^[A-Za-z ]+$'),
+-- Tabella prodotto
+CREATE TABLE IF NOT EXISTS prodotto (
+    codprodotto SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL CHECK (nome ~* '^[A-Za-z ]+$'),
     descrizione VARCHAR(500),
-    prezzo NUMERIC NOT NULL DEFAULT 0.0,
+    prezzo NUMERIC DEFAULT 0.00 CHECK (prezzo >= 0.00),
     luogoprovenienza VARCHAR(255),
     dataraccolta DATE,
     datamungitura DATE,
     glutine BOOLEAN,
     datascadenza DATE,
-    categoria VARCHAR(255),
+    categoria TIPOLOGIA,
     scorta INT CHECK (scorta >= 0),
-    CONSTRAINT categoria CHECK (
+    CONSTRAINT categoria_check CHECK (
         (categoria = 'Ortofrutticoli' AND dataraccolta IS NOT NULL AND datamungitura IS NULL AND datascadenza IS NULL AND glutine IS NULL) OR
         (categoria = 'Latticini' AND dataraccolta IS NULL AND datamungitura IS NOT NULL AND datascadenza IS NULL AND glutine IS NULL) OR
         (categoria = 'Inscatolati' AND dataraccolta IS NULL AND datamungitura IS NULL AND datascadenza IS NOT NULL AND glutine IS NULL) OR
@@ -55,30 +54,32 @@ CREATE TABLE prodotto(
     )
 );
 
--- Definizione della tabella ordine
-
-CREATE TABLE ordine(
-    CodOrdine VARCHAR(5) PRIMARY KEY, CHECK(CodOrdine ~* '^[0-9]+$'),
-    PrezzoTotale NUMERIC NOT NULL DEFAULT 0.0, CHECK(PrezzoTotale >= 0.0),
-    DataAcquisto DATE NOT NULL,
-    CodCliente VARCHAR(5) NOT NULL,
-    CodDipendente VARCHAR(5) NOT NULL,
-    CONSTRAINT OrdineClienteFK FOREIGN KEY(CodCliente) REFERENCES CLIENTE(CodCliente),
-    CONSTRAINT OrdineDipendenteFK FOREIGN KEY(CodDipendente) REFERENCES DIPENDENTE(CodDipendente)
+-- Tabella ordine
+CREATE TABLE IF NOT EXISTS ordine (
+    codordine SERIAL PRIMARY KEY,
+    prezzototale REAL NOT NULL DEFAULT 0.00 CHECK (prezzototale >= 0),
+    dataacquisto DATE NOT NULL,
+    codcliente INTEGER NOT NULL,
+    coddipendente INTEGER NOT NULL,
+    CONSTRAINT ordineclientefk FOREIGN KEY (codcliente) REFERENCES cliente (codcliente)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
+    CONSTRAINT ordinedipendentefk FOREIGN KEY (coddipendente) REFERENCES dipendente (coddipendente)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION
 );
 
--- Definizione della tabella articoliordine
-
-CREATE TABLE articoliordine(
-    CodOrdine VARCHAR(5) NOT NULL,
-    CodProdotto VARCHAR(5) NOT NULL,
-    CodCliente VARCHAR(5) NOT NULL,
-    prezzo NUMERIC NOT NULL DEFAULT 0.00,
-    numeropunti NUMERIC NOT NULL DEFAULT 0.00,
+-- Tabella articoliordine
+CREATE TABLE IF NOT EXISTS articoliordine (
+    codordine INTEGER NOT NULL,
+    codprodotto INTEGER NOT NULL,
+    codcliente SERIAL PRIMARY KEY,  -- Colonna auto-incrementata che diventa chiave primaria
+    prezzo NUMERIC NOT NULL DEFAULT 0.00 CHECK (prezzo >= 0.00),
+    numeropunti NUMERIC NOT NULL DEFAULT 0.00 CHECK (numeropunti >= 0.00),
     numeroarticoli INT NOT NULL,
-    categoria VARCHAR(255),
-    CONSTRAINT articoliordinecliente FOREIGN KEY (codcliente) REFERENCES cliente (codcliente),
-    CONSTRAINT articoliordineprodotto FOREIGN KEY (codprodotto) REFERENCES prodotto (codprodotto),
-    CONSTRAINT articoliordineordine FOREIGN KEY (codordine) REFERENCES ordine (codordine)
+    categoria TIPOLOGIA,
+    CONSTRAINT articoliordineclientefk FOREIGN KEY (codcliente) REFERENCES cliente (codcliente),
+    CONSTRAINT articoliordineprodottofk FOREIGN KEY (codprodotto) REFERENCES prodotto (codprodotto),
+    CONSTRAINT articoliordineordinek FOREIGN KEY (codordine) REFERENCES ordine (codordine)
 );
 
