418dd74735d7b072cb0534b1c0bd17daf812b8bb
Kenobi <59702902+Kenobi1797@users.noreply.github.com>
2025-06-22 21:20:53 +0200

    refactor: semplifica l'installazione dei pacchetti e migliora la gestione dei servizi

--- Ubuntu-Utility/InstallApp.sh
+++ Ubuntu-Utility/InstallApp.sh
@@ -3,60 +3,32 @@ set -e
 
 echo "Aggiornamento del sistema..."
 sudo apt update && sudo apt upgrade -y
+sudo apt install -y curl imagemagick \
+  ca-certificates gnupg lsb-release wget apt-transport-https \
+  zram-config preload flatpak git gparted \
+  clamav clamtk postgresql vlc cmake aptitude doxygen graphviz net-tools \
+  gdebi dos2unix texlive-latex-base texlive-latex-extra git-lfs cryptsetup \
+  lvm2 synaptic stacer build-essential libvips-dev jest \
+  linux-tools-common linux-tools-generic gnome-tweaks \
+  default-jdk redis tor hplip hplip-gui
 
-# Installa curl (se non è già presente)
-sudo apt install -y curl
-
-# Aggiorna i pacchetti e installa ImageMagick
-sudo apt update
-sudo apt install imagemagick
-
-# Definizione della lista dei pacchetti da installare
-packages=(
-  ca-certificates curl gnupg lsb-release wget apt-transport-https
-  zram-config preload flatpak git gparted
-  clamav clamtk postgresql git
-  vlc cmake aptitude doxygen graphviz net-tools
-  gdebi dos2unix texlive-latex-base texlive-latex-extra git-lfs cryptsetup
-  lvm2 synaptic stacer build-essential libvips-dev jest
-  linux-tools-common linux-tools-generic
-  gnome-tweaks
-)
-
-echo "Installazione dei pacchetti: ${packages[*]}"
-sudo apt install -y "${packages[@]}"
-
-sudo apt install default-jdk
-
-sudo apt update
-sudo apt install redis
-sudo apt install tor
-sudo apt install hplip hplip-gui
-
-# Installa git-lfs e inizializza
-sudo apt install -y git-lfs
 git lfs install
 
 echo "Configurazione della password PostgreSQL..."
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
-echo "Avvio e abilitazione dei servizi Tor e Redis..."
-sudo systemctl start tor
-sudo systemctl enable tor
-sudo systemctl start redis-server
-sudo systemctl enable redis-server
+# Avvio e abilitazione servizi Tor e Redis
+sudo systemctl start tor && sudo systemctl enable tor
+sudo systemctl start redis-server && sudo systemctl enable redis-server
 
 # Funzione per scaricare, installare e pulire un pacchetto .deb
 install_deb() {
   local url=$1
   local file=$(basename "$url")
-
   echo "Scaricamento di $file..."
   wget -q --show-progress "$url" -O "$file"
-
   echo "Installazione di $file..."
   sudo dpkg -i "$file" || sudo apt-get install -f -y
-
   echo "Pulizia di $file..."
   rm -f "$file"
   echo "$file installato correttamente."
@@ -85,7 +57,7 @@ sudo snap install scrcpy
 echo "Installazione di Docker..."
 sudo snap install docker
 sudo snap start docker
-sudo groupadd docker
+sudo groupadd docker || true
 sudo usermod -aG docker "$USER"
 
 echo "Installazione completata!"
