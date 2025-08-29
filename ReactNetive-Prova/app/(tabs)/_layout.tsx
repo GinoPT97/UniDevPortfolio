9e7f6b264ca971748756266e8ea91e6e86094ce5
Kenobi <59702902+Kenobi1797@users.noreply.github.com>
2025-08-29 11:02:18 +0200

    fix: rimuovi la sezione "Step 2: Explore" dal componente HomeScreen

--- ReactNetive-Prova/app/(tabs)/_layout.tsx
+++ ReactNetive-Prova/app/(tabs)/_layout.tsx
@@ -29,12 +29,13 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
+            // Usa uno sfondo trasparente su iOS per mostrare l'effetto blur
             position: 'absolute',
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
