8e214883142e78e7274e938dec813239fac5cea0
Kenobi <59702902+Kenobi1797@users.noreply.github.com>
2025-08-29 09:19:06 +0200

    fix: rimuovi import non utilizzato e correggi la mappatura delle icone in IconSymbol

--- ReactNetive-Prova/components/ui/IconSymbol.tsx
+++ ReactNetive-Prova/components/ui/IconSymbol.tsx
@@ -1,7 +1,7 @@
 // Fallback for using MaterialIcons on Android and web.
 
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { SymbolWeight, SymbolViewProps } from 'expo-symbols';
+import { SymbolViewProps } from 'expo-symbols';
 import { ComponentProps } from 'react';
 import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
 
@@ -18,7 +18,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
-} as IconMapping;
+  'calculator': 'calculate',
+} as unknown as IconMapping;
 
 /**
  * An icon component that uses native SF Symbols on iOS, and Material Icons on Android and web.
@@ -30,12 +31,11 @@ export function IconSymbol({
   size = 24,
   color,
   style,
-}: {
+}: Readonly<{
   name: IconSymbolName;
   size?: number;
   color: string | OpaqueColorValue;
   style?: StyleProp<TextStyle>;
-  weight?: SymbolWeight;
-}) {
+}>) {
   return <MaterialIcons color={color} size={size} name={MAPPING[name]} style={style} />;
 }
